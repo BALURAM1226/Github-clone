59444753ffd2ba4d869b2bf0c92dcb4c145825fe
Kumavat Baluram <72209634+BALURAM1226@users.noreply.github.com>
2021-06-10 16:25:49 +0530

    Update Navbar.jsx

--- src/Components/homepage/Navbar.jsx
+++ src/Components/homepage/Navbar.jsx
@@ -10,21 +10,19 @@ export default function Navbar(props){
   const[user] = useAuthState(auth);
   return(
     <>
-    <div className="Homepage_header">
-    <Link to="/">
-    <GitHubIcon className="github_icon"/>
-    </Link>
-    <SignOut/>
-    </div>
-  <div className="user_info">
-  
-  <img className="user_img" src={props.userpic} alt="user_pic"/>
-  <div className = "user_names">
-  <h3>{user.displayName}</h3>
-  <h4 className="user_login_name">{props.username}</h4>
-  </div>
-  </div>
-  </>
-
+<div className="Homepage_header">
+   <Link to="/">
+   <GitHubIcon className="github_icon"/>
+   </Link>
+   <SignOut/>
+</div>
+<div className="user_info">
+   <img className="user_img" src={props.userpic} alt="user_pic"/>
+   <div className = "user_names">
+      <h3>{user.displayName}</h3>
+      <h4 className="user_login_name">{props.username}</h4>
+   </div>
+</div>
+</>
   )
 }
